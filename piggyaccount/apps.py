c06d5f4319a0c67b52dd34a3284b7420666317e2
totalnoMartina <totalnobzvz@yahoo.com>
2023-01-21 22:47:39 +0000

    Add piggyaccount instead account

--- piggyaccount/apps.py
+++ piggyaccount/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AccountConfig(AppConfig):
+class PiggyaccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'account'
+    name = 'piggyaccount'
