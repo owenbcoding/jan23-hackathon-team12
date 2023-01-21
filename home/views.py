290fe1c0afe5f671c80289880939e946b3271fd5
eoghankb <eoghankb@gmail.com>
2023-01-21 14:23:42 +0000

    created function for about page view

--- home/views.py
+++ home/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def homepage(request):
-    return render(request, 'base.html')
+    return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to return the about page """
+    return render(request, 'home/about.html')
