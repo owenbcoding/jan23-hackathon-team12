e63522b8ec62521c835518444087e129b825fbd7
ChrisT-CC <cristaecc@gmail.com>
2023-01-21 13:47:58 +0000

    Add basic models for account app

--- account/models.py
+++ account/models.py
@@ -1,3 +1,58 @@
+''' Models for account app '''
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    """Account model"""
+
+    class Meta:
+        """
+        Adjust the verbose name or the plural form of it from
+        the Django defaults
+        """
+        verbose_name_plural = 'Accounts'
+    
+    def __str__(self):
+        return str(self.name)
+
+
+class Wallet(models.Model):
+    """Wallet model"""
+
+    class Meta:
+        """
+        Adjust the verbose name or the plural form of it from
+        the Django defaults
+        """
+        verbose_name_plural = 'Wallets'
+    
+    def __str__(self):
+        return str(self.name)
+
+
+class Transaction(models.Model):
+    """Transaction model"""
+
+    class Meta:
+        """
+        Adjust the verbose name or the plural form of it from
+        the Django defaults
+        """
+        verbose_name_plural = 'Transactions'
+    
+    def __str__(self):
+        return str(self.name)
+
+
+class Category(models.Model):
+    """Category model"""
+
+    class Meta:
+        """
+        Adjust the verbose name or the plural form of it from
+        the Django defaults
+        """
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return str(self.name)
